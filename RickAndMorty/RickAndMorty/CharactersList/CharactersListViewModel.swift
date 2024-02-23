aab958132de5bd9642bba47decd04b0952b60767
Omar Ali <omar.moh.aly@gmail.com>
2024-02-24 00:08:57 +0100

    Added List of characters

---
+++ RickAndMorty/RickAndMorty/CharactersList/CharactersListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CharactersListViewModel.swift
+//  RickAndMorty
+//
+//  Created by Omar Ali on 23/2/24.
+//
+
+import Foundation
---
+++ RickAndMorty/RickAndMorty/Models/Character.swift
@@ -0,0 +1,8 @@
+//
+//  Character.swift
+//  RickAndMorty
+//
+//  Created by Omar Ali on 23/2/24.
+//
+
+import Foundation
