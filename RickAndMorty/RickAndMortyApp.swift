636935e22577d1145372646dfbb70b9bf270caeb
Omar Ali <omar.moh.aly@gmail.com>
2024-02-23 20:47:34 +0100

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Omar Ali on 23/2/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
